5b82b54a7ad6d5d57afce3e98a3d7d669e0235e5
Renzo Silva <renzoasilva@gmail.com>
2022-08-01 21:52:19 -0400

    first html

--- students.js
+++ students.js
@@ -6,10 +6,20 @@ const port = 4001
 app.use(cors())
 app.use(express.json())
 
+let students = ['bob', 'lily', 'charly', 'samantha']
+
 app.get("/", (req, res) => {
-    res.send("hello world")
+    let output = "<html><body><ul>" //start
+    //output = output + "hello there thml" //middle
+    for (let i=0; i < students.lenght; i++)
+    output = output + 
+    '<li><input type="checkbox">' +
+    students[i] + '</li>'
+
+    output = output + "</ul></body></html>" //end
+    res.send(output)
 })
 
 app.listen(port, () => {
-    console.log("I was needed")
+    console.log("Localhost:4001")
 })
